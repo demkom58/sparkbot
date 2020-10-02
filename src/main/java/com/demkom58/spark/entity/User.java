59346f0c04b464d1df62027f59a7447bed6d08c0
demkom58 <demkom58@gmail.com>
2020-10-02 05:56:36 +0300

    optimize: remove unused import

--- src/main/java/com/demkom58/spark/entity/User.java
+++ src/main/java/com/demkom58/spark/entity/User.java
@@ -3,7 +3,6 @@ package com.demkom58.spark.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.*;
 
