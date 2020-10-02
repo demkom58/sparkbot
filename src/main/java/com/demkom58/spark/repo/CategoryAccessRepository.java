60f709782a6ef16c952dd65a5eb8c35273301339
demkom58 <demkom58@gmail.com>
2020-10-02 06:03:00 +0300

    repository: added new request with sql query to check user's access to category

--- src/main/java/com/demkom58/spark/repo/CategoryAccessRepository.java
+++ src/main/java/com/demkom58/spark/repo/CategoryAccessRepository.java
@@ -3,14 +3,24 @@ package com.demkom58.spark.repo;
 import com.demkom58.spark.entity.Category;
 import com.demkom58.spark.entity.CategoryAccess;
 import com.demkom58.spark.entity.Group;
-import org.springframework.data.repository.CrudRepository;
+import com.demkom58.spark.entity.User;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 
-public interface CategoryAccessRepository extends CrudRepository<CategoryAccess, Long> {
+public interface CategoryAccessRepository extends JpaRepository<CategoryAccess, Long> {
     CategoryAccess getById(Long id);
 
     Collection<CategoryAccess> getAllByCategory(Category category);
 
     Collection<CategoryAccess> getAllByGroup(Group group);
+
+    @Query("""
+            SELECT (COUNT(ga.group.id) > 0) FROM GroupAccess ga WHERE ga.user = ?2 AND ga.group.id IN (
+                SELECT ca.group.id FROM CategoryAccess ca WHERE ca.category.id = ?1
+            )
+            """)
+    boolean hasUserAccess(@NotNull final Category category, @NotNull final User user);
 }
